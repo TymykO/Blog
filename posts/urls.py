316336401dc78249c5fcbde1e9fc8bd45896f1b2
LloydMG <szymon.zawadzki55@gmail.com>
2025-02-02 14:58:26 +0100

    urlsy

--- posts/urls.py
+++ posts/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 from . import views
+app_name = "posts"
+urlpatterns = [
+    path("", views.posts_list, name="list"),
+    path("<int:post_id>/", views.post_details, name="details"),
+    path("new/", views.post_create, name="create"),
 
+]
