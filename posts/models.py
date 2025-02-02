6243fc58fb1ff2a6b2406289059df013b2999cfa
TymykO <152806676+TymykO@users.noreply.github.com>
2025-02-02 14:30:17 +0100

    add Post to models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.author} - {self.title}'
